5620a7414a4d60f32cd7f30a364952bba39584dc
ThomasWagner <ThomasWagner-dev@users.noreply.github.com>
2020-11-18 19:20:32 +0100

    Worldsize change
    Changed Worldsize

--- DungeonWorld.java
+++ DungeonWorld.java
@@ -13,7 +13,7 @@ public class DungeonWorld extends World
     public DungeonWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+            super(1425, 850, 1); 
         dmgMultiplier = FileWork.getDmgMultiplier();
     }
     
