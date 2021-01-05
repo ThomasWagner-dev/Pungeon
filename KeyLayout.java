61bbe6b0d82e0db5814afe03ca22f3662475d999
KommentatorForAll <46420447+KommentatorForAll@users.noreply.github.com>
2021-01-05 15:13:26 +0100

    added functiongroups to keylayout
    added functiongroups to later show up when booting the game

--- KeyLayout.java
+++ KeyLayout.java
@@ -20,9 +20,33 @@ public class KeyLayout
         w_cycle_b = "x",
         i_cycle_f = "f",
         i_cycle_b = "r";
-        
+
+    enum ActionGroup {
+        MOVEMENT,
+        USE,
+        ATTACK,
+        ITEMS,
+        WEAPONS
+    }
     /**
      * TODO: open remap window for remapping
      */
     public void remap(){}
+
+    public String getKeysOfActionGroup(ActionGroup ag) {
+        switch (ag) {
+            case MOVEMENT:
+                return String.join(",", new String[] {up, down, left, right, run, sneak});
+            case USE:
+                return use;
+            case ATTACK:
+                return attack;
+            case ITEMS:
+                return String.join(",", new String[] {i_cycle_f, i_cycle_b});
+            case WEAPONS:
+                return String.join(",", new String[] {w_cycle_f, w_cycle_b});
+            default:
+                return "";
+        }
+    }
 }
